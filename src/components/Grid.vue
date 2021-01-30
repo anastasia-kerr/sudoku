db058bdcfb4cf3761cb7c787c7cf83b96de6a10b
anastasia-kerr <38504863+anastasia-kerr@users.noreply.github.com>
2021-01-30 15:13:42 +0000

    fixed the repeatted numbers int he row/column issue (#1)

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,64 +1,100 @@
 <template>
   <v-container>
-    <template v-if="gridIsReady">
-    <v-row v-for="num in count" :key="num">
-      <v-chip v-for="number in grid[num]" :key="getKey(number,num)">{{number}}</v-chip>
-    </v-row>
-    </template>
+    <v-card width="300">
+      <v-card-text>
+        <template v-if="gridIsReady">
+          <v-row v-for="num in count" :key="num">
+            <v-chip v-for="number in grid[num]" :key="getKey(number, num)">{{
+              number
+            }}</v-chip>
+          </v-row>
+        </template>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
 <script>
 export default {
   name: "Region",
- data: function () {
+  data: function () {
     return {
+      counter: 0,
       grid: [],
       count: 9,
-      gridIsReady:false,
+      gridIsReady: false,
     };
   },
   created() {
     this.initializeGrid();
   },
-  mounted(){
+  mounted() {
     this.populateGrid();
-    this.gridIsReady=true;
+    this.gridIsReady = true;
   },
   methods: {
-    getKey(number,index)
-    {
-      return `${number}+${index}+${Date.now()}`
+    getKey(number, index) {
+      return `${this.getRandom(0, 99992)}- ${number}+${index}+${Date.now()}`;
     },
     getRandom(min = 1, max = 9) {
       let num = Math.random() * (max - min) + min;
       return Math.round(num);
     },
-    populateRow(index) {
-      let i = 0;
-      while (i <9) {
-        let random = this.getRandom();
-        if(this.isInTheColumn(i,random) ||this.isInTheRow(i,random))
-        {
-          continue
-        }
-        this.grid[index][i]=random;
-        i++
+    generateArrayOfNumbers(numbers) {
+      return [...Array(numbers).keys()].slice(1);
+    },
+    resetColumn(index) {
+      for (let i = 0; i < 8; i++) {
+        let clone = [...this.grid[i]];
+        clone[index] = null;
+        this.$set(this.grid, i, clone);
       }
+    },
+    populateColumn(index) {
+      this.resetColumn(index);
+      let i = 0;
+      let numbers = this.generateArrayOfNumbers(10);
+      let numberOfTries = 0;
+      while (i < 9) {
+        let indexOfNumbers = this.getRandom(0, numbers.length - 1);
+        let random = numbers[indexOfNumbers];
+        if (!this.isInTheRow(i, random) && !this.isInTheColumn(index, random)) {
+          let clone = [...this.grid[i]];
+          clone[index] = random;
+          this.$set(this.grid, i, clone);
+          numbers.splice(indexOfNumbers, 1);
+          numberOfTries = 0;
+          i++;
+        } else {
+          numberOfTries++;
+        }
+        if (numberOfTries >= 8) {
+          numberOfTries = 0;
+          this.resetColumn(index);
+          numbers = this.generateArrayOfNumbers(10);
+          i = 0;
+        }
+      }
+      this.counter++;
+    },
+    reset() {
+      let i = 0;
+      while (i < this.count) {
+        this.resetColumn(i);
+        i++;
+      }
+      this.populateGrid();
     },
     populateGrid() {
-      let i;
-      for  (i = 0; i < 9; i++){
-      this.populateRow(i);
+      for (let i = 0; i < 9; i++) {
+        this.populateColumn(i);
       }
     },
-    isInTheRow(index,item) {
-     const foundItem = this.grid[index].find(x=>x === item);
-      return foundItem;
-   },
+    isInTheRow(index, item) {
+      return this.grid[index].some((x) => x === item);
+    },
     isInTheColumn(index, item) {
-      const foundItem = this.grid.find(x=>x[index] === item)
-      return foundItem;
+      return this.grid.some((x) => x[index] === item);
     },
     initializeGrid() {
       let i = 0;
